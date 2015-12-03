36c000b94796f6891c00c773ea1aeeb5ad9bf4a8
unknown <dchougule@Tavwitpd032.internal.tavisca.com>
2015-12-03 19:03:35 +0530

    HashSet Added with more functions, encapsulation added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var collection={};
 var HashMap=require("./lib/HashMap");
+var HashSet=require("./lib/HashSet");
 
 collection.HashMap=HashMap.HashMap;
+collection.HashSet=HashSet.HashSet;
 
 module.exports=collection;
